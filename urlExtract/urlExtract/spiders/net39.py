ab66c3faf2a8677520c67dbc08d4e1ccc559014f
tenaghirmb <tenag_hirmb@hotmail.com>
2017-11-09 21:10:58 +0800

    2017/11/9 start scrapy project

---
+++ urlExtract/urlExtract/spiders/net39.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from urlExtract.items import UrlextractItem
+
+
+class Net39Spider(CrawlSpider):
+    name = 'net39'
+    allowed_domains = ['39.net']
+    start_urls = ['http://39.net/']
+
+    rules = (
+        Rule(LinkExtractor(allow_domains=('39.net',)), callback='parse_url', follow=False),
+    )
+
+    def parse_url(self, response):
+        if len(response.url) < 100:
+            item = UrlextractItem()
+            item['site'] = 'net39'
+            item['url'] = response.url
+            yield item
