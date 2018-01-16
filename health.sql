1c6f1e6b0cb49f5a0c8c19bdb78dc03d938e194c
AKA <tenag_hirmb@hotmail.com>
2018-01-16 17:31:27 +0800

    2018/1/16 health SQL file

---
+++ health.sql
@@ -0,0 +1,56 @@
+/*
+电信数据处理
+健康医疗
+*/
+
+
+-- bulk insert
+BULK INSERT splitfile1 FROM 'C:\data\lte\splitfile1.txt'  
+  WITH (  
+      FIELDTERMINATOR = '\t',  
+      ROWTERMINATOR = '\n'  
+  );  
+
+
+-- batch bulk insert
+declare @count int
+declare @spath varchar(500)
+set @count=5
+while @count<=330
+begin
+	set @spath = '''C:\data\lte\splitFile' + cast(@count as varchar(50)) + '.txt'''
+	exec('BULK INSERT lte FROM ' + @spath + 'WITH (FIELDTERMINATOR = ''\t'', ROWTERMINATOR = ''\n'')')
+	set @count = @count + 1
+end
+
+
+-- 新增列、修改列
+ALTER TABLE lte   
+ADD urldomain varchar(500) null  
+
+ALTER TABLE lte   
+ADD timestamp1 datetime null
+
+alter table lte 
+alter column userid varchar(100) null
+
+alter table lte 
+alter column timestamp varchar(50) null
+
+
+-- 变换unix时间戳、提取url的域名
+update [lte]
+set urldomain=LEFT(url,isnull(CHARINDEX('/',url)-1),
+	timestamp1=DATEADD(S,CAST( SUBSTRING(timestamp,1,10) AS INT ) + 8 * 3600,'1970-01-01 00:00:00')
+where url is not null and CHARINDEX('/',url)>1
+
+
+-- 查看数据
+SELECT TOP 1000 [userid]
+      ,[timestamp]
+      ,[url]
+      ,[agent]
+      ,[ref]
+      ,[date]
+  FROM [data].[dbo].[lte]
+
