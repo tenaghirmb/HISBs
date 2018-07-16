8f00371a5bc2dc2910742375121e79379af1e76c
TENAG HIRMB <tenag_hirmb@hotmail.com>
2018-07-16 12:36:29 +0800

    2018/7/16 topic transformation corrected

--- fsqca/segmentation.py
+++ fsqca/segmentation.py
@@ -2,7 +2,7 @@
 # @Author: aka
 # @Date:   2018-06-20 17:05:08
 # @Last Modified by:   aka
-# @Last Modified time: 2018-06-22 20:30:01
+# @Last Modified time: 2018-07-16 12:24:27
 # @Email: tenag_hirmb@hotmail.com
 
 import pymysql.cursors
@@ -13,16 +13,19 @@ import textwrap
 from scipy import stats
 import math
 import os
+import logging
 
-connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password=os.environ.get('mysql_password', '960728'),
-                             db='hdf',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+logging.basicConfig(filename='calibration.log', level=logging.INFO)
+
 
 def query(sql):
     try:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password=os.environ.get('mysql_password', '960728'),
+                                     db='hdf',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
         with connection.cursor() as cursor:
             cursor.execute(sql)
             result = cursor.fetchall()
@@ -30,28 +33,32 @@ def query(sql):
     finally:
         connection.close()
 
+
 def dist(sr, srname='Please assign variable name'):
     try:
         plt.figure()
         sr.plot.hist(bins=80)
     except AttributeError:
         sr = pd.Series(sr)
-        sr.plot.hist(bins=1000)
+        sr.plot.hist(bins=80)
     plt.title(srname)
     plt.show()
     return None
 
+
 def bcdist(sr, srname='Please assign variable name'):
-    if sr.min()==0:
-        sr = sr.apply(lambda x: x+0.01)
+    if sr.min() == 0:
+        sr = sr.apply(lambda x: x + 0.01)
     data, _ = stats.boxcox(sr)
+    return dist(data, srname)
+
+
+def logdist(sr, base=10, srname='Please assign variable name'):
+    if sr.min() == 0:
+        sr = sr.apply(lambda x: x + 0.01)
+    sr = sr.apply(lambda x: math.log(x, base))
     return dist(sr, srname)
 
-def logdist(sr, srname='Please assign variable name'):
-    if sr.min()==0:
-        sr = sr.apply(lambda x: x+0.01)
-    sr = sr.apply(lambda x: math.log(x, 10))
-    return dist(sr, srname)
 
 sql = textwrap.dedent("""
     SELECT
@@ -73,10 +80,11 @@ sql = textwrap.dedent("""
     FROM ultra_ultimate;
 """)
 df = query(sql)
-df = df.astype({'ME':'float','MC':'float','CS':'float','MAP':'float','OP':'float','F':'float','POP':'float','SOP':'float','DIS':'float',})
+df = df.astype({'ME': 'float', 'MC': 'float', 'CS': 'float', 'MAP': 'float', 'OP': 'float', 'F': 'float', 'POP': 'float', 'SOP': 'float', 'DIS': 'float', })
 
-df.usefulre = df.usefulre.apply(lambda x: math.log(x+0.01, 10))
-df.comment_score = df.comment_score.apply(lambda x: math.log(x+0.01, 10))
+df.iloc[:, 0:9] = df.iloc[:, 0:9].div(df.iloc[:, 0:9].max(axis=1), axis='index')
+df.usefulre = df.usefulre.apply(lambda x: math.log(x + 0.01, 10))
+df.comment_score = df.comment_score.apply(lambda x: math.log(x + 0.01, 10))
 df.number_of_comments = df.number_of_comments.apply(lambda x: math.log(x, 10))
 
 
@@ -84,16 +92,18 @@ df.number_of_comments = df.number_of_comments.apply(lambda x: math.log(x, 10))
 # dist(df.number_of_comments, 'Number of Comments')
 # dist(df.usefulre, 'Comment Usefulness')
 
+
 def segment(df, condition, cname):
-    for n, x in df.groupby([df['disease_cat'],condition]):
+    for n, x in df.groupby([df['disease_cat'], condition]):
         doccap = 'high' if n[1] else 'low'
         filename = n[0] + '_' + cname + '_' + doccap + '.csv'
         x.reset_index(drop=True, inplace=True)
-        x.to_csv(filename, columns = ['ME', 'MC', 'CS', 'MAP', 'OP', 'F', 'POP', 'SOP', 'DIS', 'usefulre'])
+        logging.info(filename + '\n' + str(x.usefulre.quantile([0.95, 0.5, 0.05])))
+        x.to_csv(filename, columns=['ME', 'MC', 'CS', 'MAP', 'OP', 'F', 'POP', 'SOP', 'DIS', 'usefulre'])
 
 
 os.chdir('data')
-segment(df, df['doctorProfession'].isin(['副主任医师','主任医师']), 'title')
-segment(df, df['hospital_grade'].isin(['三甲','三级']), 'grade')
+segment(df, df['doctorProfession'].isin(['副主任医师', '主任医师']), 'title')
+segment(df, df['hospital_grade'].isin(['三甲', '三级']), 'grade')
 segment(df, df['number_of_comments'] > df.number_of_comments.median(), 'noc')
 segment(df, df['comment_score'] > df.comment_score.median(), 'cs')
